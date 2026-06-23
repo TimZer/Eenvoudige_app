23b4e8909cbcd03723650557954353d20636036a
TimZer <tim.degoeij@edu-kw1c.nl>
2026-06-23 12:15:25 +0200

    hier worden je gegevens opgeslagen, opgehaald, alles verwijderen en kijkt of je gegevens bestaan

---
+++ javascript/opslag.js
@@ -0,0 +1,94 @@
+/*
+FUNCTIE: Gegevens opslaan
+*/
+function slaGegevensOp(
+    lijstMetSlaapGegevens
+) {
+
+    // Zet de lijst om naar tekst
+    let gegevensAlsTekst =
+        JSON.stringify(
+            lijstMetSlaapGegevens
+        );
+
+    // Sla de tekst op in LocalStorage
+    localStorage.setItem(
+        "opgeslagenSlaapGegevens",
+        gegevensAlsTekst
+    );
+
+}
+
+/*
+FUNCTIE: Gegevens ophalen
+*/
+function haalGegevensOp() {
+
+    // Haal gegevens op uit LocalStorage
+    let opgeslagenGegevens =
+        localStorage.getItem(
+            "opgeslagenSlaapGegevens"
+        );
+
+    // Controleer of er gegevens bestaan
+    if (
+        opgeslagenGegevens === null
+    ) {
+
+        // Geef lege lijst terug
+        return [];
+
+    } else {
+
+        // Zet tekst terug naar een lijst
+        let lijstMetSlaapGegevens =
+            JSON.parse(
+                opgeslagenGegevens
+            );
+
+        // Geef lijst terug
+        return lijstMetSlaapGegevens;
+
+    }
+
+}
+
+/*
+FUNCTIE: Verwijder alle gegevens
+*/
+function verwijderAlleGegevens() {
+
+    // Verwijder gegevens uit LocalStorage
+    localStorage.removeItem(
+        "opgeslagenSlaapGegevens"
+    );
+
+}
+
+/*
+FUNCTIE: Controleer of gegevens bestaan
+*/
+function controleerOfGegevensBestaan() {
+
+    // Haal gegevens op uit opslag
+    let opgeslagenGegevens =
+        localStorage.getItem(
+            "opgeslagenSlaapGegevens"
+        );
+
+    // Controleer of gegevens aanwezig zijn
+    if (
+        opgeslagenGegevens === null
+    ) {
+
+        // Er zijn geen gegevens
+        return false;
+
+    } else {
+
+        // Er zijn wel gegevens
+        return true;
+
+    }
+
+}
